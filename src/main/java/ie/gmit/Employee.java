a01f578c8c5d07bbff50949471da085eebedab84
SHANE HIGGINS - Student <G00339730@gmit.ie>
2020-03-09 10:13:42 +0000

    Employee name and id test commit

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -1,4 +1,45 @@
 package ie.gmit;
+/*
+    Shane Higgins
+    Software and test in class test
+    09/03/2020
+    G00339730@gmit.ie
+ */
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Employee {
+    private String name;
+    private int EmployeeNum;
+
+    public Employee(String name, int EmployeeNum) {
+
+        this.name = name;
+        this.EmployeeNum = EmployeeNum;
+    }
+
+    public int getEmployeeNum() {
+        EmployeeNum = 22541;
+        return EmployeeNum;
+    }
+
+    public Employee(String invalid) {
+    }
+
+    public String getName() {
+
+        name = "Shane";
+        return name;
+    }
+
+    public void setName(String name) {
+
+        if (name.length()>=5){
+            assertThrows(IllegalArgumentException.class,() ->
+                    new Employee("Invalid"));
+        }else {
+            this.name = name;
+        }
+    }
+
 }
+
--- src/test/java/ie/gmit/EmployeeTest.java
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -1,5 +1,39 @@
 package ie.gmit;
+/*
+    Shane Higgins
+    Software and test in class test
+    09/03/2020
+    G00339730@gmit.ie
+ */
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EmployeeTest {
 
+   @Test
+   public void nameCheck(){
+       Employee employee = new Employee("Shane");
+       assertEquals("Shane",employee.getName());
+   }
+
+   @Test
+   public void EmployeeNumCheck(){
+        Employee employee = new Employee("shane",22541);
+        assertEquals(22541,employee.getEmployeeNum());
+   }
+
+   @Test
+   public void NameSizeTOBig(){
+       assertThrows(IllegalArgumentException.class, () -> new Employee("shaneq"));
+
+   }
+
+
+
+   @Test
+   public void EmployeeNumToBig(){
+       assertThrows(IllegalArgumentException.class, () ->new Employee("Invalid Size of Employee number"));
+   }
 }
